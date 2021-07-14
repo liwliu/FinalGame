3c24cc186aa377284e8c5bd8ccc19524a1592408
liwliu <liwliu@ucsc.edu>
2021-07-14 13:40:33 -0700

    Added Preload.js to preload all assets

---
+++ src/scenes/Preload.js
@@ -0,0 +1,15 @@
+/* Class: Preload.js
+
+This class is used for preloading all assets needed for the game to operate
+
+*/
+
+class Preload extends Phaser.Scene{
+    constructor() {
+        super("preloadScene");
+    }
+
+    preload() {
+
+    }
+}
