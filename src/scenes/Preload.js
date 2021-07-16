b885fe128da82af0ce29f962beb2e651b6e3dc90
John <jsanli@ucsc.edu>
2021-07-15 21:30:15 -0700

    attempting to fix assets on pages

--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -29,10 +29,10 @@ class Preload extends Phaser.Scene{
         this.load.image('checkpoint', './assets/img/checkpoint.png');
         this.load.image('chest', './assets/img/treasureChest.png');
         this.load.image('meleeEnemy', './assets/img/satanCornChip.png');
-        this.load.image('enemyProjectile', '/assets/img/enemyProjectile.png');
-        this.load.image('rock', '/assets/img/rock.png');
-        this.load.spritesheet('blue', '/assets/img/pressButton.png', {frameWidth: 64, frameHeight: 64, startFrame:0, endFrame:1});
-        this.load.spritesheet('red', '/assets/img/holdButton.png', {frameWidth: 64, frameHeight: 64, startFrame:0, endFrame:1});
+        this.load.image('enemyProjectile', './assets/img/enemyProjectile.png');
+        this.load.image('rock', './assets/img/rock.png');
+        this.load.spritesheet('blue', './assets/img/pressButton.png', {frameWidth: 64, frameHeight: 64, startFrame:0, endFrame:1});
+        this.load.spritesheet('red', './assets/img/holdButton.png', {frameWidth: 64, frameHeight: 64, startFrame:0, endFrame:1});
     }
 
     create() {
