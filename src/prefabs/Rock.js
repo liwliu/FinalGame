bb06dbba967a937d0c4d6e31938d5337b3fdb5a0
John <jsanli@ucsc.edu>
2021-07-19 18:53:39 -0700

    rock support square 3

--- src/prefabs/Rock.js
+++ src/prefabs/Rock.js
@@ -5,33 +5,67 @@ class Rock extends Phaser.GameObjects.Sprite {
     }
 
     update() {
-        if(this.checkCollision(window.Level1Square4.explorer, this)){
-            if(keyW.isDown) {
-                this.y-=5;
-                if(this.y < 5){
-                    this.y+=5;
-                    window.Level1Square4.explorer.y+= 5;
-                }
-            }
-            if(keyA.isDown) {
-                this.x-=5;
-                if(this.x < 0){
-                    this.x+=5;
-                    window.Level1Square4.explorer.x+= 5;
-                }
-            }
-            if(keyD.isDown) {
-                this.x+=5;
-                if(this.x > 970){
-                    this.x-=5;
-                    window.Level1Square4.explorer.x-= 5;
-                }
-            }
-            if(keyS.isDown) {
-                this.y+=5;
-                if(this.y > 965){
+        if(game.settings.screen == 14){
+           if(this.checkCollision(window.Level1Square4.explorer, this)){
+               if(keyW.isDown) {
                     this.y-=5;
-                    window.Level1Square4.explorer.y-= 5;
+                    if(this.y < 5){
+                        this.y+=5;
+                        window.Level1Square4.explorer.y+= 5;
+                    }
+                }
+                if(keyA.isDown) {
+                    this.x-=5;
+                    if(this.x < 0){
+                        this.x+=5;
+                        window.Level1Square4.explorer.x+= 5;
+                    }
+                }
+                if(keyD.isDown) {
+                    this.x+=5;
+                    if(this.x > 970){
+                        this.x-=5;
+                        window.Level1Square4.explorer.x-= 5;
+                    }
+                }
+                if(keyS.isDown) {
+                    this.y+=5;
+                    if(this.y > 965){
+                        this.y-=5;
+                        window.Level1Square4.explorer.y-= 5;
+                    }
+                }
+            }
+        }
+        else if(game.settings.screen == 13){
+           if(this.checkCollision(window.Level1Square3.explorer, this)){
+               if(keyW.isDown) {
+                    this.y-=5;
+                    if(this.y < 5){
+                        this.y+=5;
+                        window.Level1Square3.explorer.y+= 5;
+                    }
+                }
+                if(keyA.isDown) {
+                    this.x-=5;
+                    if(this.x < 0){
+                        this.x+=5;
+                        window.Level1Square3.explorer.x+= 5;
+                    }
+                }
+                if(keyD.isDown) {
+                    this.x+=5;
+                    if(this.x > 970){
+                        this.x-=5;
+                        window.Level1Square3.explorer.x-= 5;
+                    }
+                }
+                if(keyS.isDown) {
+                    this.y+=5;
+                    if(this.y > 965){
+                        this.y-=5;
+                        window.Level1Square3.explorer.y-= 5;
+                    }
                 }
             }
         }
